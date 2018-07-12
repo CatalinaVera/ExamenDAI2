e2da542d31cc9fad21ed480ca420c6bbada77008
Catalina Vera <ca.verah@alumnos.duoc.cl>
2018-07-12 17:07:55 -0400

    Sesiones aplicadas *-*

--- controladorLoginFuncionarios.php
+++ controladorLoginFuncionarios.php
@@ -4,9 +4,13 @@ if (isset($_POST['Ingresar']))
 {
 	if($_POST['txtUsuario']=='12345678-9' and $_POST['txtClave']=='123')
 	{
-        header("Status: 301 Moved Permanently");
-		header("Location: http://localhost/examendai2/perfilAdministrador.html");
-		exit;
+		session_start();
+		$username = $_POST['txtUsuario'];
+
+		$_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+
+		header("Location: http://localhost/examendai2/perfilAdministrador.php");
 	}
 	else
 	{
